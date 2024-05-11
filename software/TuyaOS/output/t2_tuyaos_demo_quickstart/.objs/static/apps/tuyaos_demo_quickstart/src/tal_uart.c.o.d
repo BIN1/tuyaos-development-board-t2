667a9aa9be84fb217379c53fcfc6a482b8d2623e
BIN1 <jian01bin@163.com>
2024-05-11 17:04:20 +0800

    de

---
+++ software/TuyaOS/output/t2_tuyaos_demo_quickstart/.objs/static/apps/tuyaos_demo_quickstart/src/tal_uart.c.o.d
@@ -0,0 +1,24 @@
+/home/jianbin/tuya/T2U/tuyaos-development-board-t2/software/TuyaOS/output/t2_tuyaos_demo_quickstart/.objs/static/apps/tuyaos_demo_quickstart/src/tal_uart.c.o: \
+ /home/jianbin/tuya/T2U/tuyaos-development-board-t2/software/TuyaOS/apps/tuyaos_demo_quickstart/src/tal_uart.c \
+ include/components/tal_driver/include/tal_uart.h \
+ include/adapter/utilities/include/tuya_cloud_types.h \
+ include/base/include/tuya_iot_config.h \
+ include/base/include/tuya_error_code.h \
+ include/adapter/init/include/tkl_init_common.h \
+ include/adapter/uart/include/tkl_uart.h \
+ include/adapter/rtc/include/tkl_rtc.h \
+ include/adapter/watchdog/include/tkl_watchdog.h \
+ include/adapter/adc/include/tkl_adc.h \
+ include/adapter/gpio/include/tkl_gpio.h \
+ include/adapter/flash/include/tkl_flash.h \
+ include/adapter/pwm/include/tkl_pwm.h \
+ include/adapter/timer/include/tkl_timer.h \
+ include/adapter/i2c/include/tkl_i2c.h \
+ include/adapter/spi/include/tkl_spi.h \
+ include/components/tal_system/include/tal_mutex.h \
+ include/components/tal_system/include/tal_semaphore.h \
+ include/adapter/utilities/include/tuya_ringbuf.h \
+ include/adapter/utilities/include/tuya_cloud_types.h \
+ include/adapter/utilities/include/tuya_slist.h \
+ include/components/tal_system/include/tal_log.h \
+ include/components/tal_system/include/tal_memory.h
